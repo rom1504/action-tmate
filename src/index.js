0e51156cd3404471f753013a5b03f0103afd3424
Romain Beaumont <romain.rom1@gmail.com>
2021-01-20 09:43:36 +0000

    fix console log

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ import { execShellCommand } from "./helpers"
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export async function run() {
-    console.log('hi')
   const optionalSudoPrefix = core.getInput('sudo') === "true" ? "sudo " : "";
   const installCommand = core.getInput('installCommand');
   try {
-    core.debug("Installing dependencies")
+    console.debug("Installing dependencies")
     if (installCommand !== '' && installCommand !== undefined) {
-        await Promise.race([execShellCommand(installCommand), new Promise(resolve => setTimeout(resolve, 60000))]);
+        await execShellCommand(installCommand);
     }
     else if (process.platform === "darwin") {
       await execShellCommand('brew install tmate');
@@ -24,43 +23,42 @@ export async function run() {
       await execShellCommand(optionalSudoPrefix + 'apt-get update');
       await execShellCommand(optionalSudoPrefix + 'apt-get install -y tmate openssh-client');
     }
-    console.log('hello')
-    core.debug("Installed dependencies successfully");
+    console.debug("Installed dependencies successfully");
 
     if (process.platform !== "win32") {
-      core.debug("Generating SSH keys")
+      console.debug("Generating SSH keys")
       fs.mkdirSync(path.join(os.homedir(), ".ssh"), { recursive: true })
       try {
         await execShellCommand(`echo -e 'y\n'|ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa`);
       } catch { }
-      core.debug("Generated SSH-Key successfully")
+      console.debug("Generated SSH-Key successfully")
     }
 
-    core.debug("Creating new session")
+    console.debug("Creating new session")
     await execShellCommand('tmate -S /tmp/tmate.sock new-session -d');
     await execShellCommand('tmate -S /tmp/tmate.sock wait tmate-ready');
     console.debug("Created new session successfully")
 
-    core.debug("Fetching connection strings")
+    console.debug("Fetching connection strings")
     const tmateSSH = await execShellCommand(`tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'`);
     const tmateWeb = await execShellCommand(`tmate -S /tmp/tmate.sock display -p '#{tmate_web}'`);
 
     console.debug("Entering main loop")
     const continuePath = process.platform !== "win32" ? "/continue" : "C:/msys64/continue"
     while (true) {
-      core.info(`WebURL: ${tmateWeb}`);
-      core.info(`SSH: ${tmateSSH}`);
+      console.info(`WebURL: ${tmateWeb}`);
+      console.info(`SSH: ${tmateSSH}`);
 
       const skip = fs.existsSync(continuePath) || fs.existsSync(path.join(process.env.GITHUB_WORKSPACE, "continue"))
       if (skip) {
-        core.info("Existing debugging session because '/continue' file was created")
+        console.info("Existing debugging session because '/continue' file was created")
         break
       }
       await sleep(5000)
     }
   } catch (error) {
-    core.info('failed')
-    core.info(error.message)
+    console.info('failed')
+    console.info(error.message)
     core.setFailed(error.message);
   }
 }
